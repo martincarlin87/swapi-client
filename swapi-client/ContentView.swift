068dfc5dc75150c025c9fa4a76be6096cf7ddfbb
Martin Carlin <martin.carlin@thecyberhawk.com>
2020-06-08 20:34:24 +0100

    Initial Commit

---
+++ swapi-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swapi-client
+//
+//  Created by Martin Carlin on 08/06/2020.
+//  Copyright © 2020 Martin Carlin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
